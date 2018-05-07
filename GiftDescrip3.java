cd5fc4c8e695e94b86eeb2ce441059d15da14364
Neela123 <39058457+Neela123@users.noreply.github.com>
2018-05-07 21:09:56 +0530

    Add files via upload

---
+++ GiftDescrip3.java
@@ -0,0 +1,22 @@
+package com.example.shilpa.springblossoms;
+
+
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+public class GiftDescrip3 extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.gift3_description);
+    }
+}
